712f2e7244b5d53d6c4a3386af8667a1224cbcc2
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-10-27 20:07:05 +0500

    add seeding data in user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do |i|
+  User.find_or_create_by(name: "Alpha", email: "alpha#{i}@gmail.com")
+end
