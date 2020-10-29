2178f160813ab4c589e25dd834dbcc7e1afa9654
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-10-29 16:30:25 +0500

    add paperclip migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates  :email, uniqueness: true, presence: true
-  
+
   has_one_attached :picture 
+  has_one_attached :avatar 
+  #  styles: { medium: "300x300>", thumb: "100x100>" }
+  # validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_130941) do
+ActiveRecord::Schema.define(version: 2020_10_29_082202) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,10 @@ ActiveRecord::Schema.define(version: 2020_10_28_130941) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.bigint "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
