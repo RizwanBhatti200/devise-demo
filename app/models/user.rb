9f22fbe48170d2270e9a58b6fe1e597a41516c31
RizwanBhatti200 <rizwanqaiserbhatti200@gmail.com>
2020-10-29 12:44:16 +0500

    upload and show user picture through active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates  :email, uniqueness: true, presence: true
+  
+  has_one_attached :picture 
 end
